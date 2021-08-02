e826fe6d6ea9fd5cf9a6ee716c8fbc7fd02804e6
Fazil Amirli <amirli.fazil@student.atilim.edu.tr>
2021-08-02 10:02:07 +0300

    Added methods to use swagger for testing BookController

--- src/main/java/com/fz/bookstoreapp/config/SpringFoxConfig.java
+++ src/main/java/com/fz/bookstoreapp/config/SpringFoxConfig.java
@@ -1,23 +1,47 @@
 package com.fz.bookstoreapp.config;
 
+import com.fz.bookstoreapp.controllers.BookController;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Author:Fazil Amirli
  * Created on: 7/27/2021
  */
 @Configuration
+@EnableSwagger2
+@ComponentScan(basePackageClasses = BookController.class)
+@PropertySource("classpath:swagger.properties")
 public class SpringFoxConfig {
+    private static final String SWAGGER_API_VERSION = "1.0";
+    private static final String LICENSE_TEXT = "License";
+    private static final String title = "Books REST API";
+    private static final String description = "RESTful API for Books";
+
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .license(LICENSE_TEXT)
+                .version(SWAGGER_API_VERSION)
+                .build();
+    }
+
     @Bean
-    public Docket api() {
+    public Docket bookAPI(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
                 .select()
-                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
